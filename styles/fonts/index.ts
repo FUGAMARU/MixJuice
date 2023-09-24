0427f52f6b85e2123078f2a8600c0ee8e97cfbf4
FUGAMARU <gofugashi@gmail.com>
2023-09-24 18:47:09 +0900

    [Fix] GreycliffCFフォントの指定ファイル名が間違っていたのを修正

--- styles/fonts/index.ts
+++ styles/fonts/index.ts
@@ -15,9 +15,9 @@ export const greycliffCF = localfont({
     { path: "GreycliffCF-Light.woff2", weight: "300", style: "normal" },
     { path: "GreycliffCF-Regular.woff2", weight: "400", style: "normal" },
     { path: "GreycliffCF-Medium.woff2", weight: "500", style: "normal" },
-    { path: "GreycliffCF-Demibold.woff2", weight: "600", style: "normal" },
+    { path: "GreycliffCF-DemiBold.woff2", weight: "600", style: "normal" },
     { path: "GreycliffCF-Bold.woff2", weight: "700", style: "normal" },
-    { path: "GreycliffCF-Extrabold.woff2", weight: "800", style: "normal" }
+    { path: "GreycliffCF-ExtraBold.woff2", weight: "800", style: "normal" }
   ],
   variable: "--font-greycliff-cf"
 })
