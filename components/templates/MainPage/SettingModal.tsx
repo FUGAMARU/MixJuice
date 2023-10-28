4c37ce672ab33c876c502f71ee9b8a09281b2d0c
FUGAMARU <gofugashi@gmail.com>
2023-10-29 01:10:22 +0900

    [Update] 設定モーダルのボタンのレイアウトを変更

--- components/templates/MainPage/SettingModal.tsx
+++ components/templates/MainPage/SettingModal.tsx
@@ -35,59 +35,47 @@ const SettingModal = ({ isOpen, onClose }: Props) => {
     defaultValue: DEFAULT_SETTING_VALUES
   })
 
-  const modalHeader = useMemo(() => {
-    if (breakPoint === "SmartPhone")
+  const footerFunctions = useMemo(() => {
+    if (breakPoint === "SmartPhone") {
       return (
-        <Stack w="100%" justify="center" align="center">
-          <Group spacing="xs">
-            <PiUserCircleThin size="2.5rem" />
-            <Text fw={700}>{userInfo?.email}</Text>
-          </Group>
-
-          <Stack w="100%" spacing="xs">
-            <Button w="100%" variant="outline" size="xs">
+        <Stack spacing="xs">
+          <Group spacing="xs" sx={{ justifyContent: "center" }} grow>
+            <Button size="xs" variant="outline" color="gray">
               メールアドレス変更
             </Button>
-            <Group w="100%" grow spacing="xs">
-              <Button variant="outline" size="xs">
-                パスワード変更
-              </Button>
-              <Button variant="outline" size="xs">
-                退会
-              </Button>
-            </Group>
-          </Stack>
+            <Button size="xs" variant="outline" color="gray">
+              パスワード変更
+            </Button>
+          </Group>
+          <Group spacing="xs" sx={{ justifyContent: "center" }} grow>
+            <Button size="xs" variant="outline" color="gray">
+              アカウント削除
+            </Button>
+            <Button size="xs" variant="outline" color="gray">
+              キャッシュ削除
+            </Button>
+          </Group>
         </Stack>
       )
+    }
 
     return (
-      <Flex align="center" justify="space-between">
-        <Group spacing="0.3rem">
-          <PiUserCircleThin size="2.5rem" />
-          <Text
-            w="18.5rem"
-            fw={700}
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap"
-            }}
-          >
-            {userInfo?.email}
-          </Text>
-        </Group>
-
-        <Group spacing="xs">
-          <Button variant="outline" size="xs">
-            ユーザー情報変更
-          </Button>
-          <Button variant="outline" size="xs">
-            サインアウト
-          </Button>
-        </Group>
-      </Flex>
+      <Group spacing="xs" sx={{ justifyContent: "center" }} grow>
+        <Button size="xs" variant="outline" color="gray">
+          メールアドレス変更
+        </Button>
+        <Button size="xs" variant="outline" color="gray">
+          パスワード変更
+        </Button>
+        <Button size="xs" variant="outline" color="gray">
+          アカウント削除
+        </Button>
+        <Button size="xs" variant="outline" color="gray">
+          キャッシュ削除
+        </Button>
+      </Group>
     )
-  }, [breakPoint, userInfo?.email])
+  }, [breakPoint])
 
   return (
     <ModalDefault
@@ -100,7 +88,26 @@ const SettingModal = ({ isOpen, onClose }: Props) => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      {modalHeader}
+      <Flex align="center" justify="space-between">
+        <Group spacing="0.3rem">
+          <PiUserCircleThin size="2.5rem" />
+          <Text
+            maw="27rem"
+            fw={700}
+            sx={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap"
+            }}
+          >
+            {userInfo?.email}
+          </Text>
+        </Group>
+
+        <Button variant="outline" size="xs">
+          サインアウト
+        </Button>
+      </Flex>
 
       <Divider my="sm" />
 
@@ -108,9 +115,7 @@ const SettingModal = ({ isOpen, onClose }: Props) => {
         {Object.entries(SETTING_ITEMS)
           .filter(
             ([key]) =>
-              key !== "VERSION" &&
-              key !== "THEME" &&
-              key !== "BACKGROUND_OF_QUEUE"
+              !["VERSION", "THEME", "BACKGROUND_OF_QUEUE"].includes(key)
           ) // TODO: テーマやキューの背景の実装が完了したら解放する
           .map(([key, val]) => (
             <Flex
@@ -148,14 +153,7 @@ const SettingModal = ({ isOpen, onClose }: Props) => {
       <Divider my="sm" />
 
       <Box ta="center">
-        <Group spacing="xs" sx={{ justifyContent: "center" }}>
-          <Button size="xs" variant="outline" color="gray">
-            キャッシュ削除
-          </Button>
-          <Button size="xs" variant="outline" color="gray">
-            退会
-          </Button>
-        </Group>
+        {footerFunctions}
 
         <Text fz="0.7rem" color="#868e96">
           ※完了後サインアウトされます
